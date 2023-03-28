529b525200795de266a20258fcce528275e79de2
alirex <alirex.prime@gmail.com>
2023-03-28 20:36:19 +0300

    ✨ concurrency first example

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from application.concurrency_example.concurrency_example import concurrency_example
 from application.logging.init_logging import init_logging
-from application.requests_example.requests_example import requests_example
+
+
+# from application.requests_example.requests_example import requests_example
 
 
 def main() -> None:
     # print(greetings())
     # files_example_txt()
     # files_example_csv()
-    requests_example()
+    # requests_example()
+    concurrency_example()
 
 
 if __name__ == "__main__":
